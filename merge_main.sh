f39ffe3da3acdfd1187eabb10b7b7b8223ddb4bd
Antony Holmes <antony@antonyholmes.dev>
2024-02-01 16:09:07 -0500

    fix: fix: Bug fixes and updates.

--- merge_main.sh
+++ merge_main.sh
@@ -4,17 +4,39 @@
 
 
 
-# commit
-#git add -A .
-#git commit -m "Bug fixes and updates."
-#git push -u origin dev
+type="fix"
+msg="fix: Bug fixes and updates."
+branch="dev"
 
-#./commit.sh
+OPTSTRING="t:m:b:"
+
+while getopts ${OPTSTRING} opt
+do
+	case ${opt} in
+  	t)
+    	type=$OPTARG
+      	;;
+	m)
+    	msg=$OPTARG
+      	;;
+	b)
+      	branch=$OPTARG
+      	;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      exit 1
+      ;;
+  esac
+done
+
+echo "${type}: ${msg}"
+echo ${branch}
+
 
 git switch main
-git merge dev
+git merge dev -m "${type}: ${msg}"
 
 #git push -u origin main
-./commit.sh -b main
+./commit.sh -t ${type} -m ${msg} -b main
 
 git switch dev
