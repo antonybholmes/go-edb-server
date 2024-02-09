6ea2661cb9b99cc4cc77cbf89c8071b6f6b8f704
Antony Holmes <antony@antonyholmes.dev>
2024-02-09 17:11:17 -0500

    fix: Removed

--- test_urls.sh
+++ test_urls.sh
@@ -4,4 +4,8 @@ curl -v -X POST -H 'Content-Type: application/json' -d  '{"locations":[{"chr":"c
 
 curl -v "localhost:8080/dna/grch38"
 
-curl -v "localhost:8080/dna/grch38?start=1&end=100&format=lower&mask=n"
+curl -v -X POST -H 'Content-Type: application/json' -d  '{"locations":[{"chr":"chr10","start":1043441,"end":1044114}], "level":"gene", "tss":[2000, 1000]}' "localhost:8080/v1/annotate/grch38?n=5&output=text"
+
+
+
+curl -v -X POST -H 'Content-Type: application/json' -d  '{"locations":[{"chr":"chr10","start":100014303,"end":100014664}], "level":"gene", "tss":[2000, 1000]}' "https://api.rdf-lab.org/v1/annotate/grch38?n=5&output=text"
