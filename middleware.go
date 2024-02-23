e9e26e4db612b637c76e287412c685c60169923d
Antony Holmes <antony@antonybholmes.com>
2024-02-22 22:34:42 -0500

    fix: Added callbacks to standardize error handling

--- middleware.go
+++ middleware.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/antonybholmes/go-auth"
 	"github.com/antonybholmes/go-edb-api/routes"
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 	"github.com/rs/zerolog/log"
 )
@@ -52,19 +51,11 @@ import (
 
 func JwtIsAccessTokenMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		return routes.AccessTokenCB(c, func(c echo.Context, claims *auth.JwtCustomClaims) error {
 
-		user := c.Get("user").(*jwt.Token)
-		claims := user.Claims.(*auth.JwtCustomClaims)
+			log.Debug().Msgf("type: %d, %d", claims.Type, auth.TOKEN_TYPE_ACCESS)
 
-		log.Debug().Msgf("type: %d, %d", claims.Type, auth.TOKEN_TYPE_ACCESS)
-
-		//t := claims.ExpiresAt.Unix()
-		//expired := t != 0 && t < time.Now().Unix()
-
-		if claims.Type != auth.TOKEN_TYPE_ACCESS {
-			return routes.BadReq("wrong token type")
-		}
-
-		return next(c)
+			return next(c)
+		})
 	}
 }
