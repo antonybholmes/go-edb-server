34f1fdaaea70a8786bd771f1ddf1bfa97516c93a
Antony Holmes <antony@antonyholmes.dev>
2024-10-22 11:42:37 -0400

    fix: Bug fixes and updates.

--- user_mysql.sql
+++ user_mysql.sql
@@ -10,8 +10,8 @@ CREATE TABLE user_permissions (
     public_id VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL UNIQUE,
     description VARCHAR(255) NOT NULL DEFAULT "",
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL);
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL);
 CREATE INDEX roles_name_idx ON user_permissions (name);
 
 INSERT INTO user_permissions (public_id, name, description) VALUES('uwkrk2ljj387', 'Super', 'Superuser');
@@ -27,8 +27,8 @@ CREATE TABLE user_roles (
     public_id VARCHAR(255) NOT NULL UNIQUE, 
     name VARCHAR(255) NOT NULL UNIQUE,
     description VARCHAR(255) NOT NULL DEFAULT "",
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL);
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL);
 CREATE INDEX user_roles_name_idx ON user_roles (name);
 
 INSERT INTO user_roles (public_id, name) VALUES('p1gbjods0h90', 'Super');
@@ -43,8 +43,8 @@ CREATE TABLE user_roles_permissions (
     id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, 
     role_id INTEGER NOT NULL,
     permission_id INTEGER NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
     UNIQUE(role_id, permission_id),
     FOREIGN KEY(role_id) REFERENCES user_roles(id) ON DELETE CASCADE,
     FOREIGN KEY(permission_id) REFERENCES user_permissions(id) ON DELETE CASCADE);
@@ -74,9 +74,9 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL DEFAULT '',
     first_name VARCHAR(255) NOT NULL DEFAULT '',
     last_name VARCHAR(255) NOT NULL DEFAULT '',
-    email_is_verified BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL);
+    email_verified_at DATETIME DEFAULT '1000-01-01' NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL);
 CREATE INDEX users_public_id_idx ON users (public_id);
 -- CREATE INDEX name ON users (first_name, last_name);
 CREATE INDEX users_username_idx ON users (username);
@@ -88,13 +88,28 @@ CREATE TABLE users_roles (
     id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, 
     user_id INTEGER NOT NULL,
     role_id INTEGER NOT NULL, 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
     UNIQUE(user_id, role_id),
     FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY(role_id) REFERENCES user_roles(id) ON DELETE CASCADE);
 CREATE INDEX users_roles_user_id_idx ON users_roles (user_id, role_id);
 
 
-INSERT INTO users (public_id, username, email, email_is_verified) VALUES ('2ewpxwqldfl8', 'root', 'antony@antonyholmes.dev', true);
+ 
+
+-- the superuser me --
+INSERT INTO users (public_id, username, email, email_verified_at) VALUES (
+    '25bhmb459eg7',
+    'root',
+    'antony@antonyholmes.dev',
+    now()
+);
+
+-- su group --
 INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
+
+INSERT INTO users (public_id, username, email) VALUES (
+    '25bhmb459ez7',
+    'ssss',
+    'antony@antonyholmess.dev');
